c70a047d7832d2b9be344984c3eb5252f368460e
Samuel Rafini <samuelrafini@gmail.com>
2018-11-25 11:13:12 +0100

    fixed small backgroundimage issue

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,9 +4,11 @@ const Title = (props) => (
     <div className='title' style={{
         background: `url(${props.backgroundURL}) no-repeat center center fixed`,
         backgroundSize: 'cover',
+        backgroundColor: '#4A4D56'
     }}>
         <h1 className='title-header'>Weather App</h1>
     </div>
 )
 
 export default Title;
+
