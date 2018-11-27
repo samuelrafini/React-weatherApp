aa721f631f69de340981f8ba3a66d328e92b6956
Samuel Rafini <samuelrafini@gmail.com>
2018-11-27 08:31:16 +0100

    Updates

---
+++ precache-manifest.4eb817198b1e0166e8b27a5bf56ef84f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07af17b4d2f7f5bb11ae",
+    "url": "/React-weatherApp/static/js/runtime~main.07af17b4.js"
+  },
+  {
+    "revision": "354f73abf8acbd40e7a7",
+    "url": "/React-weatherApp/static/js/main.354f73ab.chunk.js"
+  },
+  {
+    "revision": "d931340e3cc097ea818a",
+    "url": "/React-weatherApp/static/js/1.d931340e.chunk.js"
+  },
+  {
+    "revision": "354f73abf8acbd40e7a7",
+    "url": "/React-weatherApp/static/css/main.abc52b82.chunk.css"
+  },
+  {
+    "revision": "f5ad8e82e4f7a81fffc87604de01cffc",
+    "url": "/React-weatherApp/index.html"
+  }
+];
